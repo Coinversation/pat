262066377b60f6c93497e46b691308f8c643afcd
bwhour <juvemeen.xio@gmail.com>
2021-04-19 17:04:19 +0800

    update selector address

--- stubs/token/pat/lib.rs
+++ stubs/token/pat/lib.rs
@@ -36,49 +36,49 @@ mod pat {
         }
 
         /// Returns the token name.
-        #[ink(message, selector = "0x6b1bb951")]
+        #[ink(message, selector = "0xd3dc9f8c")]
         pub fn token_name(&self) -> Option<String> {
             unimplemented!()
         }
 
         /// Returns the token symbol.
-        #[ink(message, selector = "0xb42c3368")]
+        #[ink(message, selector = "0xe4ab4943")]
         pub fn token_symbol(&self) -> Option<String> {
             unimplemented!()
         }
 
         /// Returns the token decimals.
-        #[ink(message, selector = "0xc64b0eb2")]
+        #[ink(message, selector = "0xc4b508e2")]
         pub fn token_decimals(&self) -> Option<u8> {
             unimplemented!()
         }
 
         /// Returns the total token supply.
-        #[ink(message, selector = "0x143862ae")]
+        #[ink(message, selector = "0x557c8bd0")]
         pub fn total_supply(&self) -> Balance {
             unimplemented!()
         }
 
         /// Returns the account balance for the specified `owner`.
-        #[ink(message, selector = "0xb7d968c9")]
+        #[ink(message, selector = "0xb13e6c24")]
         pub fn balance_of(&self, _owner: AccountId) -> Balance {
             unimplemented!()
         }
 
         /// Transfers `value` amount of tokens from the caller's account to account `to`.
-        #[ink(message, selector = "0x10d455c2")]
+        #[ink(message, selector = "0xa6b726c7")]
         pub fn transfer(&mut self, _to: AccountId, _value: Balance) -> Result<()> {
             unimplemented!()
         }
 
         /// Returns the amount which `spender` is still allowed to withdraw from `owner`.
-        #[ink(message, selector = "0xc04aa300")]
+        #[ink(message, selector = "0x984f2ea5")]
         pub fn allowance(&self, _owner: AccountId, _spender: AccountId) -> Balance {
             unimplemented!()
         }
 
         /// Transfers `value` tokens on the behalf of `from` to the account `to`.
-        #[ink(message, selector = "0xbb399017")]
+        #[ink(message, selector = "0xeb7943df")]
         pub fn transfer_from(
             &mut self,
             _from: AccountId,
@@ -90,7 +90,7 @@ mod pat {
 
         /// Allows `spender` to withdraw from the caller's account multiple times, up to
         /// the `value` amount.
-        #[ink(message, selector = "0x4ce0e831")]
+        #[ink(message, selector = "0x702770d4")]
         pub fn approve(&mut self, _spender: AccountId, _value: Balance) -> Result<()> {
             unimplemented!()
         }
